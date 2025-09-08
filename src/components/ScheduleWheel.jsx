974e4e6dff6367d88e48e044326f1409e76bf3db
Daniel Rachev <d.n.rachev@student.tudelft.nl>
2025-09-08 21:53:30 +0200

    feat: Add simple drag.

--- src/components/ScheduleWheel.jsx
+++ src/components/ScheduleWheel.jsx
@@ -1,11 +1,33 @@
+import { useLayoutEffect, useRef } from 'react';
 import TaskCard from './TaskCard';
+
+import { gsap } from 'gsap';
+import { Draggable } from 'gsap/Draggable';
+import { InertiaPlugin } from 'gsap/InertiaPlugin';
+
+gsap.registerPlugin(Draggable, InertiaPlugin);
 
 function ScheduleWheel({ schedule }) {
   const totalTasks = schedule.length;
   const angleIncrement = 360 / totalTasks;
 
+  const wheelRef = useRef(null);
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      Draggable.create(wheelRef.current, {
+        type: "rotation",
+        inertia: true,
+        edgeResistance: 0.65,
+        // resistance: 2000
+      });
+    }, wheelRef);
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <div className="schedule-wheel">
+    <div className="schedule-wheel" ref={wheelRef}>
       {schedule.map(({ task, roommate }, index) => {
         const rotation = index * angleIncrement;
         const isActive = index === 0;
